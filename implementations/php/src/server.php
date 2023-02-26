6500bbee3589ff9ca50063b011e73a84ed16d7af
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2023-02-26 08:51:43 +0300

    Use SchemaBuilder to add all directives to the schema

--- implementations/php/src/server.php
+++ implementations/php/src/server.php
@@ -6,17 +6,12 @@ namespace GraphQL\Compatibility;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Closure;
-use Exception;
+use Apollo\Federation\Enum\DirectiveEnum;
+use Apollo\Federation\SchemaBuilder;
 
-use GraphQL\Server\StandardServer;
-use GraphQL\Type\Definition\Type;
-
-use GraphQL\Compatibility\Types;
-use GraphQL\Compatibility\Type\QueryType;
 use GraphQL\Compatibility\Data\DataSource;
-
-use Apollo\Federation\FederatedSchema;
+use GraphQL\Compatibility\Type\QueryType;
+use GraphQL\Server\StandardServer;
 
 // turn off deprecation notices
 error_reporting(E_ALL ^ E_DEPRECATED);
@@ -24,15 +19,20 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 try {
     DataSource::init();
 
-    $schema = new FederatedSchema([
-        'query' => new QueryType(),
-    ]);
+    $schema = (new SchemaBuilder())->build(
+        [
+            'query' => new QueryType(),
+        ],
+        [
+            'directives' => DirectiveEnum::getAll(),
+        ],
+    );
 
     $server = new StandardServer([
         'schema' => $schema,
     ]);
 
     $server->handleRequest();
-} catch (Throwable $error) {
+} catch (\Throwable $error) {
     StandardServer::send500Error($error);
 }
